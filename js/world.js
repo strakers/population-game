242cb3687f17d40f63adaf1e1043cadbe0c72fcf
strakers <s.straker@utoronto.ca>
2026-02-20 13:16:10 -0500

    Convert js files to use ES6 imports and exports

--- js/world.js
+++ js/world.js
@@ -1,24 +1,24 @@
+import { World, Location, Person } from "./components/classes.js";
+import { System, makeHolder, countries } from "./components/globals.js";
 
 //World.prototype = Location.prototype;
 
-var moo;
-
-var world, john, sue, loli, paul, lisa, group;
+var world, john, sue, loli, paul, lisa, jack, rob, group;
 var ontario, toronto, windsor, canada, newyork, usa, uk;
 
 window.onload = function(){
 
 	 document.body.innerHTML = "";
 	 document.body.innerHTML = "<ul><li>Drag a man onto a woman to make a new person.</li><li>Drag a person onto a country to make them a resident.</li></ul><hr />";
-	 
+
 	 var personHolder = document.createElement("div");
 	 personHolder.setAttribute('id','people');
 	 document.body.appendChild( personHolder );
-	 
+
 	 var locHolder = document.createElement("div");
 	 locHolder.setAttribute('id','locations');
 	 document.body.appendChild( locHolder );
-	 
+
 	 world = new World();
 	 john = world.addPerson( "John Smith", "M", 29 );
 	 sue = world.addPerson( "Susan Robinson", "F", 26 );
@@ -30,20 +30,20 @@ window.onload = function(){
 	 jack = world.addPerson("Jack Robinson","M",39);
 	 rob = world.addPerson("Rob Ford","M",51);
 	 group = [ john, loli, paul, sue, lisa ];
-	 
+
 	 world.divideAndConquer( countries );
-	 
+
 	 /*
-	 ontario = world.addLocation('Canada').addLocation('Ontario'); 
+	 ontario = world.addLocation('Canada').addLocation('Ontario');
 	 toronto = ontario.addLocation("Toronto");
 	 windsor = ontario.addLocation("Windsor");
 	 canada = world.getLocation("Canada");
 	 newyork = world.addLocation("United States of America").addLocation("New York");
 	 usa = world.getLocation("United States of America");
 	 uk = world.addLocation("United Kingdom");
-	//sue.passAway(); 
+	//sue.passAway();
 	*/
-	
+
 	var canada = world.getLocation('Canada')
 	canada.land = document.createElement('div');
 	canada.land.className = "country can";
@@ -53,8 +53,8 @@ window.onload = function(){
 	canada.land.ondragover = function(e){ e.preventDefault(); }
 	canada.land.ondrop = function(e){
 		e.preventDefault();
-		var name = e.dataTransfer.getData("person"), 
-			person = world.getPerson(name), 
+		var name = e.dataTransfer.getData("person"),
+			person = world.getPerson(name),
 			dragTarget = person.body,
 			dropTarget = e.target,
 			countryName = dropTarget.id,
@@ -70,7 +70,7 @@ window.onload = function(){
 	canada.land.onclick = function(){ alert( 'Population: '+ canada.population() ); }
 	canada.land.place = canada;
 	document.getElementById('locations').appendChild( world.getLocation('Canada').land );
-	
+
 	var usa = world.getLocation('United States of America');
 	usa.land = document.createElement('div');
 	usa.land.className = "country can";
@@ -80,8 +80,8 @@ window.onload = function(){
 	usa.land.ondragover = function(e){ e.preventDefault(); }
 	usa.land.ondrop = function(e){
 		e.preventDefault();
-		var name = e.dataTransfer.getData("person"), 
-			person = world.getPerson(name), 
+		var name = e.dataTransfer.getData("person"),
+			person = world.getPerson(name),
 			dragTarget = person.body,
 			dropTarget = e.target,
 			countryName = dropTarget.id,
