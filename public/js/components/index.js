9e4ceff53a909513268d70343037f502d81ed8f0
strakers <s.straker@utoronto.ca>
2026-02-20 13:29:14 -0500

    Update file structure to separate dev files from public files

---
+++ public/js/components/index.js
@@ -0,0 +1,7 @@
+import { World, Location, Person } from "./classes";
+import { System, makeHolder, countries } from "./globals";
+
+export default {
+  World, Location, Person,
+  System, makeHolder, countries,
+};
