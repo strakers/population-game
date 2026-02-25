99beeb2037ea96ec92ebf22dc896b51da1b157be
strakers <s.straker@utoronto.ca>
2026-02-25 05:05:13 -0500

    Add handler for when dragging person elements

---
+++ src/scripts/events/handlers/PersonDragStartHandler.ts
@@ -0,0 +1,18 @@
+import EventHandler from "./EventHandler";
+import Person from "../../components/beings/Person";
+
+export default class PersonDragStartHandler extends EventHandler {
+  static eventName = 'dragstart';
+
+  static effect(event: DragEvent): void {
+    const person = this as unknown as Person;
+
+    if (event.dataTransfer) {
+      event.dataTransfer.setData('text/plain', person.name);
+			event.dataTransfer.setData("person", person.getDisplayElement()?.getAttribute('data-name') || '');
+      event.dataTransfer.effectAllowed = 'move';
+    } else {
+      console.warn('Drag event does not support data transfer.');
+    }
+  }
+}
